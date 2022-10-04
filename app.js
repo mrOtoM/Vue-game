c515eef012030828beb4c441fb4ed539481c9721
mrOtoM <mrotomachala@gmail.com>
2022-10-04 12:13:38 +0200

    uprava Attack a special Attack buttonuS

---
+++ app.js
@@ -0,0 +1,44 @@
+function randonValue(min, max) {
+  return Math.trunc(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.playerHealth + '%' };
+    },
+    playerBarStyles() {
+      return { width: this.monsterHealth + '%' };
+    },
+    useSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  methods: {
+    attackMonster() {
+      this.currentRound++;
+      const attackValue = randonValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = randonValue(7, 12);
+      this.playerHealth -= attackValue;
+    },
+    specialAttack() {
+      this.currentRound++;
+      const attackValue = randonValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+  },
+});
+
+app.mount('#game');
