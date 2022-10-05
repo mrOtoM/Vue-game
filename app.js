d960ba50687007fc31f685b52bc1f284a6991cd5
mrOtoM <mrotomachala@gmail.com>
2022-10-05 13:34:01 +0200

    pridal som HEAL tlacidlo do app.js

--- app.js
+++ app.js
@@ -38,6 +38,16 @@ const app = Vue.createApp({
       this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = randonValue(5, 17);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
   },
 });
 
